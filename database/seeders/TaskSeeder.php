4bfa65a402ba97f00470139f622a8cd11d23def8
Aure6 <aurelien7112001@gmail.com>
2023-12-08 11:55:17 +0100

    Reduce the number of seeded users and tasks in the database.

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -12,6 +12,6 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Task::factory(20)->create();
+        \App\Models\Task::factory(5)->create();
     }
 }
