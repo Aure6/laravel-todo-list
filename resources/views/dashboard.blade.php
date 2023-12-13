66755a06895a846f2d54803d8573f18e2aa8abd5
Aure6 <aurelien7112001@gmail.com>
2023-12-13 11:18:24 +0100

    style dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
         </div>
     @endif
 
-    <div class="py-12">
+    <div class="py-2 sm:py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm p-6 sm:rounded-lg">
                 <h1 class="text-xl">Todo List</h1>
