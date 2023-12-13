b0b5ba3820ef01ab8af1d74457806c9e20056083
Aure6 <aurelien7112001@gmail.com>
2023-12-13 11:00:52 +0100

    add comments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,6 +65,7 @@
         </form>
     </x-modal>
 
+    {{-- Default page --}}
     {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
